40ad729eecc5b79f8e11dd6c35f43a650021a5b3
MarlonVictor <cmarlonvictor11@gmail.com>
2021-08-23 15:14:49 -0300

    Implemented application responsiveness

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -8,33 +8,60 @@ import styles from './styles.module.scss'
 
 export function TransactionsTable() {
     return (
-        <section className={styles.TableContainer}>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Título <IoIosArrowDown/></th>
-                        <th>Valor <IoIosArrowDown/></th>
-                        <th>Categoria <IoIosArrowDown/></th>
-                        <th>Data <IoIosArrowDown/></th>
-                    </tr>
-                </thead>
-                
+        <>
+            <section className={styles.TableContainer}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Título <IoIosArrowDown/></th>
+                            <th>Valor <IoIosArrowDown/></th>
+                            <th>Categoria <IoIosArrowDown/></th>
+                            <th>Data <IoIosArrowDown/></th>
+                        </tr>
+                    </thead>
+                    
 
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de site</td>
-                        <td className={styles.deposit}>R$1.000,00</td>
-                        <td><BiDollar /> Venda</td>
-                        <td>10/08/2021</td>
-                    </tr>
-                    <tr>
-                        <td>t-shirt</td>
-                        <td className={styles.withdraw}>- R$100,00</td>
-                        <td><IoShirtOutline /> Roupa</td>
-                        <td>10/08/2021</td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
+                    <tbody>
+                        <tr>
+                            <td>Desenvolvimento de site</td>
+                            <td className={styles.deposit}>R$1.000,00</td>
+                            <td><BiDollar /> Venda</td>
+                            <td>10/08/2021</td>
+                        </tr>
+                        <tr>
+                            <td>t-shirt</td>
+                            <td className={styles.withdraw}>- R$100,00</td>
+                            <td><IoShirtOutline /> Roupa</td>
+                            <td>10/08/2021</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+
+            <section className={styles.MobileListContainer}>
+                <h2>Listagem</h2>
+
+                <div>
+                    <main>
+                        <p>Desenvolvimento de site</p>
+                        <span className={styles.deposit}>R$1.000,00</span>
+                    </main>
+                    <footer>
+                        <span><BiDollar /> Venda</span>
+                        <span>10/08/2021</span>
+                    </footer>
+                </div>
+                <div>
+                    <main>
+                        <p>t-shirt</p>
+                        <span className={styles.withdraw}>- R$100,00</span>
+                    </main>
+                    <footer>
+                        <span><IoShirtOutline /> Roupa</span>
+                        <span>10/08/2021</span>
+                    </footer>
+                </div>
+            </section>
+        </>
     )
 }
