4f92510f997ec86951c14710701659b6c6f52767
MarlonVictor <cmarlonvictor11@gmail.com>
2021-08-24 16:18:29 -0300

    Using formik and yup to get the form values

--- src/components/NewTransaction/index.tsx
+++ src/components/NewTransaction/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Field, Formik, Form } from 'formik'
+import { ptForm } from 'yup-locale-pt'
+import * as Yup from 'yup'
 
 import IncomeIcon from '../../assets/income.svg'
 import OutcomeIcon from '../../assets/outcome.svg'
@@ -6,35 +9,105 @@ import OutcomeIcon from '../../assets/outcome.svg'
 import styles from './styles.module.scss'
 
 
+type SubmitFormProps = {
+    name: string,
+    price: string,
+    category: string
+}
+
 export function NewTransaction() {
+    const [transactionType, setTransactionType] = useState('income')
+    
+    Yup.setLocale(ptForm)
+
+    const schema = Yup.object().shape({
+        name: Yup.string().required(),
+        price: Yup.number().integer().required(),
+        category: Yup.string().default('').required()
+    })
+
+    function submitForm(values: SubmitFormProps) {
+        const formValue = {
+            ...values,
+            transactionType
+        }
+
+        console.log(formValue)
+    }
+
     return (
-        <form className={styles.NewTransactionContainer}>
-            <h2>Novo cadastro</h2>
+        <Formik 
+            initialValues={{
+                name: '',
+                price: '',
+                category:''
+            }}
+            onSubmit={submitForm}
+            validationSchema={schema}
+            render={({ errors }) => (
+                <Form className={styles.NewTransactionContainer}>
+                    <h2>Novo cadastro</h2>
 
-            <input placeholder="Nome" />
-            <input placeholder="Valor" />
+                    <Field 
+                        name="name" 
+                        type="text" 
+                        placeholder="Nome" 
+                        className={errors.name ? styles.inputError : ''}
+                    />
+                    <small>{errors.name}</small>
 
-            <fieldset>
-                {/* <input type="checkbox" />
-                <input type="checkbox" /> */}
-                <button>
-                    <img src={IncomeIcon} alt="Entrada" />
-                    Entrada
-                </button>
-                <button>
-                    <img src={OutcomeIcon} alt="Saída" />
-                    Saída
-                </button>
-            </fieldset>
+                    <Field 
+                        name="price" 
+                        type="text" 
+                        placeholder="Valor" 
+                        className={errors.price ? styles.inputError : ''}
+                    />
+                    <small>{errors.price}</small>
 
-            <select id="cars" title="Categorias">
-                <option selected disabled>Categorias</option>
-                <option value="clothing">Roupa</option>
-            </select>
+                    <fieldset>
+                        <button 
+                            type="button"
+                            onClick={() => setTransactionType('income')}
+                            className={transactionType == 'income' ? styles.checkedButton : ''}
+                        >
+                            <img src={IncomeIcon} alt="Entrada" />
+                            Entrada
+                        </button>
+                        <button 
+                            type="button"
+                            onClick={() => setTransactionType('outcome')}
+                            className={transactionType == 'outcome' ? styles.checkedButton : ''}
+                        >
+                            <img src={OutcomeIcon} alt="Saída" />
+                            Saída
+                        </button>
+                    </fieldset>
 
-            <button className={styles.sendButton}>
-                Cadastrar
-            </button>
-        </form>
+                    <Field 
+                        as="select" 
+                        name="category" 
+                        title="Categorias"
+                        className={errors.category ? styles.inputError : ''}
+                    >
+                        <option hidden>Categorias</option>
+                        <option value="style">Moda</option>
+                        <option value="object">Objeto</option>
+                        <option value="electronics">Eletrônico</option>
+                        <option value="car">Auto</option>
+                        <option value="properties">Imóvel</option>
+                        <option value="hobby">Hobby</option>
+                        <option value="travel">Viagem</option>
+                    </Field>
+                    <small>{errors.category}</small>
+
+                    <button 
+                        type="submit"
+                        className={styles.sendButton}
+                    >
+                        Cadastrar
+                    </button>
+                </Form>
+            )}
+        />
     )
 }
